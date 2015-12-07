0a16e89c267508e2447cf741c741c14544b8fb62
gudjonss12 <gudjonss12@ru.is>
2015-12-07 10:21:32 +0000

    redundant code removed

--- QTFILES/Controller.cpp
+++ QTFILES/Controller.cpp
@@ -22,32 +22,12 @@ void Controller::listScientists(){   //function that defines how the list of Sci
     screen.howToList(select);
 
     QSqlQuery query;
-    query = sortBy(select);
+    query = sortByInSci(select);
     screen.printResult(query);
     return;
 }
 
-void Controller::addScientist(){            //function that creates a Scientist in the database
-    View screen;
-    string name = "";
-    bool sex = false;
-    QDate doB;
-    QDate doD;
-    QString fact;
 
-    screen.askName(name);
-    screen.askGender(sex);
-    doB = screen.askDateOfBirth();
-    doD = screen.askDateOfDeath();
-    screen.askFact(fact);
-
-    QString qName = QString::fromStdString(name);
-    Scientist newScientist = Scientist(qName, sex, doB, doD, fact);
-    Model db;
-    db.writeToDB(newScientist);
-
-    return;
-}
 
 void Controller::removeScientist(){      //function that finds a Scientist to erase
     View screen;
@@ -86,75 +66,9 @@ void Controller::searchScientist(){              //function that searches the da
     return;
 }
 
-void Controller::editScientist(vector<Scientist>& list){            //function that changes the information in the database
-    string editName = "";
-    QString name;
-    View screen;
-
-    screen.askName(editName);
-
-    name = QString::fromStdString(editName);
-    name = name.toLower();
-
-    QString currName = "";
-    QString doB;
-    QString doD;
-    QString fact;
-
-    int selection = 1;
-
-
-
-    bool check = false;
-
-    for(unsigned int i = 0; i < list.size(); i++){
-
-        QString temp = list[i].returnName();
-        temp = temp.toLower();
-        if(temp == name){
-            check = true;
-            screen.editSelection(selection);
-            if((selection == 1) | (selection == 6)){
-                string name = "";
-                screen.askName(name);
-                QString currName = QString::fromStdString(name);
-                list[i].setName(currName);
-            }
-            if((selection == 2) | (selection == 6)){
-                bool sex = false;
-                screen.askGender(sex);
-                list[i].setGender(sex);
-            }
-            if((selection == 3) | (selection == 6)){
-                QDate doB;
-                doB = screen.askDateOfBirth();
-                list[i].setdoB(doB);
-            }
-            if((selection == 4) | (selection == 6)){
-                QDate doD;
-                doD = screen.askDateOfDeath();
-                list[i].setdoD(doD);
-            }
-            if((selection == 5) | (selection == 6)){
-                QString fact;
-                screen.askFact(fact);
-                list[i].setFact(fact);
-            }
-        }
-    }
-    if(!check)
-        screen.nameNotFound();
-
-    Model db;
-    db.overwriteDB(list);
-
-    return;
-}
-
 void Controller::functionHandler(int n){                    //function that receives the user selection and executes accordingly
     Model db;
     View UI;
-    vector<Scientist> database = db.retDB();                //get db as a vector
     Scientist currentScientist;
     int listSelect;
     switch(n){
@@ -171,7 +85,7 @@ void Controller::functionHandler(int n){                    //function that rece
             searchFunctions();
             break;
         case 5:
-            editScientist(database);
+            //editScientist(database);
             break;
         case 6:
             //linkProgram();
@@ -262,12 +176,6 @@ void Controller::searchFunctions(){
 
 }
 
-QSqlQuery Controller::sortBy(int comp){
-    Model db;
-    QSqlQuery ret;
-    ret = db.queryList(comp);
-    return ret;
-}
 QSqlQuery Controller::sortByInSci(int comp){
     Model db;
     QSqlQuery ret;
