2c1c8d7401904cab8b7fa342f57da24e5d348989
gudjonss12 <gudjonss12@ru.is>
2015-12-08 18:33:09 +0000

    Functions organized Computer.Cpp

--- QTFILES/Computers.cpp
+++ QTFILES/Computers.cpp
@@ -13,36 +13,44 @@ Computers::Computers(int SID, QString Sname, bool Screated, int ScreationYear, Q
     creationYear = ScreationYear;
     type = Stype;
     description = Sdescription;
- }
+}
 
-QString Computers::returnName(){
-    return name;
-}
-bool Computers::returnCreated(){
-    return created;
-}
-int Computers::returnCreationYear(){
-    return creationYear;
-}
-QString Computers::returnType(){
-    return type;
-}
-QString Computers::returnDescription(){
-    return description;
-}
 void Computers::setName(QString Sname){
     name = Sname;
 }
+
 void Computers::setCreated(bool sCreated){
     created = sCreated;
 }
+
 void Computers::setCreationYear(int sCreationYear){
     creationYear = sCreationYear;
 }
+
 void Computers::setType(QString sType){
     type = sType;
 }
 
 void Computers::setDescription(QString sDescription){
     description = sDescription;
+}
+
+bool Computers::returnCreated(){
+    return created;
+}
+
+int Computers::returnCreationYear(){
+    return creationYear;
+}
+
+QString Computers::returnName(){
+    return name;
+}
+
+QString Computers::returnType(){
+    return type;
+}
+
+QString Computers::returnDescription(){
+    return description;
 }
