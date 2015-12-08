553cee515b46bf13346cc0dfe7f51ab8aaf6c174
gudjonss12 <gudjonss12@ru.is>
2015-12-08 12:34:54 +0000

    link function

--- QTFILES/Model.cpp
+++ QTFILES/Model.cpp
@@ -145,7 +145,7 @@ QSqlQuery Model::computersConnSci(int id){
     QSqlQuery ret;
     ret.prepare("SELECT people.name FROM people "
                 "INNER JOIN compGroups ON people.id = compGroups.peopleID "
-                "INNER JOIN people ON compGroups.computerID = computers.id "
+                "INNER JOIN computers ON compGroups.computerID = computers.id "
                 "WHERE computers.id = :id");
     ret.bindValue(":id", id);
     ret.exec();
