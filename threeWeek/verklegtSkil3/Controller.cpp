6bc158ec87b58f245dce10e213431b8a72bc193b
gudjonss12 <gudjonss12@ru.is>
2015-12-10 15:09:06 +0000

    redudant code removal

--- threeWeek/verklegtSkil3/Controller.cpp
+++ threeWeek/verklegtSkil3/Controller.cpp
@@ -21,36 +21,25 @@ void Controller::linkFunctions(){
     QSqlQuery query;
 
     while(which != 0){
-        UI.linkWhich(which);
         switch(which){
             case 1:
                 query = db.linkListSci();
-                UI.printResult(query);
                 break;
             case 2:
                 query = db.linkListComp();
-                UI.printResult(query);
                 break;
             case 3:
-                UI.askForSciID(currSciID);
-                UI.askForCompID(currCompID);
                 db.linkSciToComp(currSciID, currCompID);
                 break;
             case 4:
-                UI.showLinks(select);
                 if(select == 1){
-                    UI.idGet(currSciID);
                     query = db.scientistConnComp(currSciID);
-                    UI.printResult(query);
                 }
                 else if(select == 2){
-                    UI.idGet(currCompID);
                     query = db.computersConnSci(currCompID);
-                    UI.printResult(query);
                 }
                 else{
                     errorHandling();
-                    UI.invalidInput();
                 }
                 break;
             case 0:
@@ -68,29 +57,23 @@ void Controller::linkFunctions(){
 void Controller::listFunctions(){
     int which = 25;
     int select = 25;
-    View UI;
     QSqlQuery query;
 
 
     while(which != 0){
-        UI.listInterface(which);
         switch(which){
             case 1:
                 while(select != 0){
-                    select = UI.displayListFuncsSci();
                     query = sortByInSci(select);
                     if(select != 0){
-                        UI.printResult(query);
                     }
                 }
                 select = 25;
                 break;
             case 2:
                 while(select != 0){
-                    select = UI.displayListFuncsComp();
                     query = sortByInComp(select);
                     if(select != 0){
-                        UI.printResult(query);
                     }
                 }
                 select = 25;
@@ -125,23 +108,19 @@ QSqlQuery Controller::sortByInComp(int comp){
 
 void Controller::addFunctions(){
     int which = 25;
-    View UI;
     Model db;
     QSqlQuery query;
     Scientist guy;
     Computers comp;
 
     while(which != 0){
-        UI.addInterface(which);
         switch(which){
             case 1:
                 which = 25;
-                UI.populateScientist(guy);
                 db.addScientistToDatabase(guy);
                 break;
             case 2:
                 which = 25;
-                UI.populateComputer(comp);
                 db.addComputerToDatabase(comp);
                 break;
             case 0:
@@ -158,10 +137,8 @@ void Controller::addFunctions(){
 
 void Controller::removeFunctions(){
     int which = 25;
-    View UI;
 
     while(which != 0){
-        UI.removeInterface(which);
         switch(which){
             case 1:
                 which = 25;
@@ -183,22 +160,20 @@ void Controller::removeFunctions(){
     return;
 }
 
-void Controller::removeScientist(){      //function that finds a Scientist to erase
-    View screen;
+
+//function that finds a Scientist to erase
+void Controller::removeScientist(){
     Model db;
     int id;
 
-    screen.idGet(id);
     db.rmRowSci(id);
 
     return;
 }
 
 void Controller::removeComputer(){
-    View screen;
     Model db;
     int id;
-    screen.idGet(id);
     db.rmRowComp(id);
 
     return;
@@ -206,11 +181,9 @@ void Controller::removeComputer(){
 
 void Controller::searchFunctions(){
     int which = 25;
-    View UI;
     QSqlQuery query;
     int select = 25;
     while(which != 0){
-        UI.searchInterface(which);
         switch(which){
             case 1:
                 which = 25;
@@ -235,27 +208,22 @@ void Controller::searchFunctions(){
     return;
 }
 
-void Controller::searchScientistName(){              //function that searches the database
-    View screen;
+//function that searches the database
+void Controller::searchScientistName(){
     Model db;
     string name;
-    screen.askName(name);
     QSqlQuery query;
     QString qName = QString::fromStdString(name);
-    query = db.searchSciName(qName);
-    screen.printResult(query);
 
     return;
 }
 
 void Controller::searchScientistHandler(int& select){
-    View UI;
     QSqlQuery query;
     Model db;
     int secondSelect = 25;
     int currID = 0;
     while(select != 0){
-        UI.searchSecond(select);
         if(select == 1){
             searchScientistName();
         }
@@ -263,11 +231,9 @@ void Controller::searchScientistHandler(int& select){
             secondSelect = 25;
             currID = searchScientistID();
             while(secondSelect != 0){
-                UI.searchExtended(secondSelect);
                 switch(secondSelect){
                     case 1:
                         query = db.scientistConnComp(currID);
-                        UI.printResult(query);
                         secondSelect = 25;
                         break;
                     case 2:
@@ -297,15 +263,13 @@ void Controller::searchScientistHandler(int& select){
     return;
 }
 
-void Controller::searchComputerName(){              //function that searches the database
-    View screen;
+//function that searches the database
+void Controller::searchComputerName(){
     Model db;
     string name;
-    screen.askName(name);
     QSqlQuery query;
     QString qName = QString::fromStdString(name);
     query = db.searchCompName(qName);
-    screen.printResult(query);
 
     return;
 }
@@ -313,12 +277,10 @@ void Controller::searchComputerName(){              //function that searches the
 void Controller::searchComputerHandler(int& select){
     int secondSelect = 25;
     int currID = 0;
-    View UI;
     Model db;
     QSqlQuery query;
 
     while(select != 0){
-        UI.searchSecond(select);
         if(select == 1){
             select = 25;
             searchComputerName();
@@ -328,11 +290,9 @@ void Controller::searchComputerHandler(int& select){
             secondSelect = 25;
             currID = searchComputerID();
             while(secondSelect != 0){
-                UI.searchExtended(secondSelect);
                 switch(secondSelect){
                     case 1:
                         query = db.computersConnSci(currID);
-                        UI.printResult(query);
                         break;
                     case 2:
                         secondSelect = 25;
@@ -360,37 +320,28 @@ void Controller::searchComputerHandler(int& select){
 }
 
 int Controller::searchScientistID(){
-    View screen;
     Model db;
     int id;
-    screen.idGet(id);
     QSqlQuery query;
     QSqlQuery query2;
     query = db.searchSciID(id);
-
-    screen.printResult(query);
 
     return id;
 }
 
 int Controller::searchComputerID(){
-    View screen;
     Model db;
     int id;
-    screen.idGet(id);
     QSqlQuery query;
     query = db.searchCompID(id);
-    screen.printResult(query);
 
     return id;
 }
 
 void Controller::editFunctions(){
     int which = 25;
-    View UI;
 
     while(which != 0){
-    UI.editWhich(which);
     switch(which){
         case 1:
             editScientistHandler();
@@ -411,7 +362,6 @@ void Controller::editFunctions(){
 }
 
 void Controller::editSpecific(int ID, bool which){
-    View UI;
 
     if(which){
         extraScientistHandler(ID);
@@ -424,20 +374,16 @@ void Controller::editSpecific(int ID, bool which){
 }
 
 void Controller::editScientistHandler(){
-    View UI;
     int currID = 0;
 
-    UI.idGet(currID);
     editLoopScientist(currID);
 
     return;
 }
 
 void Controller::editComputerHandler(){
-    View UI;
     int currID = 0;
 
-    UI.idGet(currID);
     editLoopComputer(currID);
 
     return;
@@ -454,33 +400,26 @@ void Controller::editLoopScientist(int id){
     QString type;
     QString fact;
 
-    View UI;
     Model db;
     while(select != 0){
-        UI.editSelectionScientist(select);
         switch(select){
             case 1:
-                UI.askName(name);
                 qName = QString::fromStdString(name);
                 db.modSci(select, qName, id);
                 break;
             case 2:
-                UI.askGender(sex);
                 if(sex){
                     genderStr = "1";
                 }
                 db.modSci(select, genderStr, id);
                 break;
             case 3:
-                doB = UI.askDateOfBirth();
                 db.modSci(select, doB.toString("yyyy-MM-dd"), id);
                 break;
             case 4:
-                doD = UI.askDateOfDeath();
                 db.modSci(select,doD.toString("yyyy-MM-dd"), id);
                 break;
             case 5:
-                UI.askFact(fact);
                 db.modSci(select, fact, id);
                 break;
             case 0:
@@ -497,7 +436,6 @@ void Controller::editLoopScientist(int id){
 
 void Controller::editLoopComputer(int id){
     int select = 25;
-
     string name;
     QString qName;
     bool sex;
@@ -507,35 +445,27 @@ void Controller::editLoopComputer(int id){
     QString type;
     QString fact;
     int year;
-
-    View UI;
     Model db;
 
     while(select != 0){
-        UI.editSelectionComputer(select);
         switch(select){
             case 1:
-                UI.askName(name);
                 qName = QString::fromStdString(name);
                 db.modComp(select, qName, id);
                 break;
             case 2:
-                UI.compAskCreated(sex);
                 if(sex){
                     genderStr = "1";
                 }
                 db.modComp(select, genderStr, id);
                 break;
             case 3:
-                UI.compAskCreationDate(year);
                 db.modComp(select, QString::number(year), id);
                 break;
             case 4:
-                UI.compAskType(type);
                 db.modComp(select, type, id);
                 break;
             case 5:
-                UI.compAskDescription(fact);
                 db.modComp(select, fact, id);
                 break;
             case 0:
@@ -551,7 +481,6 @@ void Controller::editLoopComputer(int id){
 }
 
 void Controller::extraScientistHandler(int id){
-    View UI;
 
     editLoopScientist(id);
 
@@ -559,7 +488,6 @@ void Controller::extraScientistHandler(int id){
 }
 
 void Controller::extraComputerHandler(int id){
-    View UI;
 
     editLoopComputer(id);
 
@@ -567,19 +495,14 @@ void Controller::extraComputerHandler(int id){
 }
 
 void Controller::errorHandling(){
-    View UI;
-    cin.clear();
-    cin.ignore();
-    UI.invalidInput();
 
     return;
 }
 
-void Controller::functionHandler(){                    //function that receives the user selection and executes accordingly
-    View UI;
+//function that receives the user selection and executes accordingly
+void Controller::functionHandler(){
     int which = 25;
     while(which != 0){
-        UI.displayInterface(which);
         switch(which){
             case 1:
                 listFunctions();
