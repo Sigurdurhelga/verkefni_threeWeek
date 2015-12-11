a0d04814702006cd0178317c4f6acddfa7a6e4b0
gudjonss12 <gudjonss12@ru.is>
2015-12-11 09:57:23 +0000

    db connection

--- threeWeek/verklegtSkil3/main.cpp
+++ threeWeek/verklegtSkil3/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
+#include "Model.h"
+#include "Controller.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    Model db;
+    QSqlDatabase dataBase = db.openConnection();
     MainWindow w;
     w.show();
+    dataBase.close();
 
     return a.exec();
 }
