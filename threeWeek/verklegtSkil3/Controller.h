279f6ea7870fc8d826783281e04ca0b3c9ac4cf7
gudjonss12 <gudjonss12@ru.is>
2015-12-14 16:04:06 +0000

    functions ordered

--- threeWeek/verklegtSkil3/Controller.h
+++ threeWeek/verklegtSkil3/Controller.h
@@ -12,18 +12,13 @@ public:
     QVector<Scientist> getScientists(QString);
     QVector<Computers> getComputers(QString);
     QVector<QString> showLinks(QString id, bool which);
+    QVector<QString> getNameForLinks(bool which);
 
     void add(QString, QString, QString, QString, QString, bool);   
     void remove(QString, bool);
     void search(QString, bool);
     void edit(QString id, QString newThing, int column, bool which);
-<<<<<<< HEAD
-
-=======
     void link(QString id, QString name, bool which);
-    QVector<QString> getNameForLinks(bool which);
-    QVector<QString> showLinks(QString id, bool which);
->>>>>>> 84c5c2c3b8bab87d48f0ee3e12315d455b7808e9
 };
 
 #endif // CONTROLLER_H
