2614af8923939da061521508bd72924f5dd6d0ac
gudjonss12 <gudjonss12@ru.is>
2015-12-14 10:48:23 +0000

    Controller.h cleaned up

--- threeWeek/verklegtSkil3/Controller.h
+++ threeWeek/verklegtSkil3/Controller.h
@@ -8,51 +8,14 @@
 class Controller
 {
 public:
-
+    // These functions store info from the database in a vector to make it easier to work with.
     QVector<Scientist> getScientists(QString);
     QVector<Computers> getComputers(QString);
 
-    // List functions
-    void linkFunctions();
-    void listFunctions();
-    QSqlQuery sortByInSci(int comp);
-    QSqlQuery sortByInComp(int comp);
-
-    // Add functions
-    void add(QString, QString, QString, QString, QString, bool);
-    void addFunctions();
-
-    // Remove functions
+    void add(QString, QString, QString, QString, QString, bool);   
     void remove(QString, bool);
-    void removeFunctions();
-    void removeScientist();
-    void removeComputer();
-
-    // Search functions
     void search(QString, bool);
-    void searchFunctions();
-    void searchScientistName();
-    void searchScientistHandler(int& select);
-    void searchComputerName();
-    void searchComputerHandler(int& select);
-    int searchScientistID();
-    int searchComputerID();
-
-
-    // Edit functions
     void edit(QString id, QString newThing, int column, bool which);
-    void editFunctions();
-    void editSpecific(int, bool);
-    void editScientistHandler();
-    void editComputerHandler();
-    void editLoopScientist(int id);
-    void editLoopComputer(int id);
-    void extraScientistHandler(int id);
-    void extraComputerHandler(int id);
-
-    // Misc
-    void errorHandling();
-    void functionHandler();
 };
 
 #endif // CONTROLLER_H
